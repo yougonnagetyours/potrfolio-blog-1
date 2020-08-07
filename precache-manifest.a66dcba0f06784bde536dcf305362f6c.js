3b5b490bd2fa294047c9fc82269f2d588dbf8ae4
Michal Potoczny <michal.potoczny.92@gmail.com>
2020-08-07 18:13:15 +0200

    Updates

---
+++ precache-manifest.a66dcba0f06784bde536dcf305362f6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae72ce5f5eeb6c3b5955",
+    "url": "/potrfolio-blog-1/static/css/main.c44c85a7.chunk.css"
+  },
+  {
+    "revision": "ae72ce5f5eeb6c3b5955",
+    "url": "/potrfolio-blog-1/static/js/main.8d11f7c9.chunk.js"
+  },
+  {
+    "revision": "d7895d39aeaa736e3b47",
+    "url": "/potrfolio-blog-1/static/js/runtime~main.54744e19.js"
+  },
+  {
+    "revision": "4a0e6e664e243c85cb38",
+    "url": "/potrfolio-blog-1/static/css/2.5773182c.chunk.css"
+  },
+  {
+    "revision": "4a0e6e664e243c85cb38",
+    "url": "/potrfolio-blog-1/static/js/2.b1ef8b6f.chunk.js"
+  },
+  {
+    "revision": "463e7292fc5c153f47e8fedb5904bdd2",
+    "url": "/potrfolio-blog-1/static/media/IMG_2852.463e7292.jpeg"
+  },
+  {
+    "revision": "05b56dd059e4fce9e1e7595481f28571",
+    "url": "/potrfolio-blog-1/static/media/IMG_2863.05b56dd0.jpeg"
+  },
+  {
+    "revision": "b1d21144ea2e86b64cc08879fdab94fd",
+    "url": "/potrfolio-blog-1/static/media/IMG_2865.b1d21144.jpeg"
+  },
+  {
+    "revision": "1f2081925d936d72a60ddb10496041d0",
+    "url": "/potrfolio-blog-1/static/media/IMG_2867.1f208192.jpeg"
+  },
+  {
+    "revision": "74ec28f2a20fb1ff6bffea8e0e2c90d8",
+    "url": "/potrfolio-blog-1/static/media/IMG_2885.74ec28f2.jpeg"
+  },
+  {
+    "revision": "d37781f3c97ad9fb5c342ab3cbb1c6c9",
+    "url": "/potrfolio-blog-1/static/media/IMG_3386.d37781f3.jpeg"
+  },
+  {
+    "revision": "0c2cb20f63ca75a656e7e2f4328d4bed",
+    "url": "/potrfolio-blog-1/index.html"
+  }
+];
